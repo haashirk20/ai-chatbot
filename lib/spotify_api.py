d77acb552687c798be5dcce4785b4ca956987feb
haashirk2003 <haashirk2003@gmail.com>
2024-02-19 19:11:24 -0500

    added soundcloud playlist convert feature

--- lib/spotify_api.py
+++ lib/spotify_api.py
@@ -1,11 +1,16 @@
-from auth import spotify_client_id, spotify_client_secret, redirect_uri
 import spotipy
 import secrets
 import string
+import json
+from sclib import SoundcloudAPI, Track, Playlist
 
-
-def get_playlist_link(playlist_id):
-    sp = spotipy.Spotify(
+def auth():
+    with open("./lib/auth.json") as f:
+        data = json.load(f)
+        spotify_client_id = data["spotify_client_id"]
+        spotify_client_secret = data["spotify_client_secret"]
+        redirect_uri = data["redirect_uri"]
+    return spotipy.Spotify(
         auth_manager=spotipy.SpotifyOAuth(
             client_id=spotify_client_id,
             client_secret=spotify_client_secret,
@@ -13,6 +18,9 @@ def get_playlist_link(playlist_id):
             scope="playlist-modify-public",
         )
     )
+
+def get_playlist_link(playlist_id):
+    sp = auth()
     playlist = sp.playlist(playlist_id)
     return playlist.get("external_urls").get("spotify")
 
@@ -24,16 +32,13 @@ def get_songs(query, numSongs):
         songs.append(i["uri"])
     return songs
 
+def fetch_song(artist, songname):
+    sp = auth()
+    query = artist + " " + songname
+    return sp.search(q=query, limit=1, type="track")["tracks"]["items"][0]["uri"]
 
 def get_similar_songs(query, numSongs):
-    sp = spotipy.Spotify(
-        auth_manager=spotipy.SpotifyOAuth(
-            client_id=spotify_client_id,
-            client_secret=spotify_client_secret,
-            redirect_uri=redirect_uri,
-            scope="playlist-modify-public",
-        )
-    )
+    sp = auth()
     # get id of first result of query and find recommendations
     id = sp.search(q=query, limit=1, type="track")["tracks"]["items"][0]["id"]
     return sp.recommendations(seed_tracks=[id], limit=numSongs)
@@ -50,7 +55,7 @@ def get_recommendation(query, numSongs):
     )
 
 
-def create_playlist(query, numSongs):
+def create_playlist(query, songs):
     # initializing size of string
     N = 5
     # using secrets.choice()
@@ -59,15 +64,7 @@ def create_playlist(query, numSongs):
         secrets.choice(string.ascii_uppercase + string.digits) for i in range(N)
     )
     playlist_name = query + res
-    songs = get_songs(query, numSongs)
-    sp = spotipy.Spotify(
-        auth_manager=spotipy.SpotifyOAuth(
-            client_id=spotify_client_id,
-            client_secret=spotify_client_secret,
-            redirect_uri=redirect_uri,
-            scope="playlist-modify-public",
-        )
-    )
+    sp = auth()
     user_id = sp.current_user()["id"]
     sp.user_playlist_create(user_id, playlist_name)
     playlists = sp.current_user_playlists()
@@ -76,3 +73,18 @@ def create_playlist(query, numSongs):
             playlist_id = i["id"]
     sp.playlist_add_items(playlist_id, songs)
     return get_playlist_link(playlist_id)
+
+def convert_playlist(query):
+    api = SoundcloudAPI()
+    try:
+        pl = api.resolve(query)
+    except:
+        return "Invalid URL"
+
+    assert type(pl) is Playlist
+
+    songs = []
+    for track in pl.tracks:
+        songs.append(fetch_song(track.artist, track.title))
+
+    return create_playlist(pl.title, songs)
